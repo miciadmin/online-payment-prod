16123487b0e3c82cb91c98c69cb1267cef24b34e
tramillimart <salomonlimart@gmail.com>
2024-04-25 15:09:51 +0800

    6th update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/home'
 import PaymentStep1 from './pages/payment_step1'
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import reactRefresh  from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite'
+import react  from '@vitejs/plugin-react'
 
-export default {
-  plugins: [reactRefresh ()],
+export default defineConfig({
+  plugins: [react ()],
   build: {
     rollupOptions: {
       external: ['react-router-dom']
     }
   }
-}
+})
