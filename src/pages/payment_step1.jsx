c656d8cb93c22823106cebba57bdababcaa57beb
tramillimart <salomonlimart@gmail.com>
2024-04-26 15:10:53 +0800

    25th update

--- src/pages/payment_step1.jsx
+++ src/pages/payment_step1.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 import Step1 from "../component/step1";
 
 export default function() {
+    
+    useEffect(() => {
+        const handlePopstate = () => {
+            if (location.pathname === "/search-policy") {
+              window.history.go(-(window.history.length - 1));
+            }
+        };
+        window.addEventListener("popstate", handlePopstate);
+    }, []);
+
     return(
         <Step1/>
     )
