22760b2e768f3960fe00ed50cfddb183b0c0be5e
tramillimart <salomonlimart@gmail.com>
2024-05-02 08:57:40 +0800

    38th update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ export default {
   plugins: [reactRefresh()],
   optimizeDeps: {
     include: ['react', 'react-dom']
-  }
+  },
+  base: process.env.NODE_ENV === 'production' ? '/micpayuat.mici.com.ph/' : '/'
 }
