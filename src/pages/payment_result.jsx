3da4cc8723997230ebf172c7a7d8fec34a8841b7
tramillimart <salomonlimart@gmail.com>
2024-05-10 16:07:55 +0800

    86th update

--- src/pages/payment_result.jsx
+++ src/pages/payment_result.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect} from 'react';
 import ResultPage from "../component/result";
+import { endpoint } from '../js/utils';
 
 export default function() {
     const [params, setParams] = useState({});
